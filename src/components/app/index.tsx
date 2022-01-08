a36d9d13312ab53e4ad610b7526751d6243c690b
Clayton Rafael <clayton.rssouza@gmail.com>
2022-01-08 12:20:31 -0300

    feat: add and config  miragejs

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { http } from '../../services/api';
 import { Title } from './styles';
 
 const App: React.FC = () => {
+  const [users, setUsers] = useState();
+
+  useEffect(() => {
+    http.get('/users').then(({ data }) => setUsers(data.users));
+  }, []);
+
   return (
     <div>
-      <Title>Meu app</Title>
+      <Title>{JSON.stringify(users)}</Title>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import './index.css';
+import { MockServer } from './services/miragejs/MockServer';
+
+const environment = process.env.NODE_ENV;
+if (environment !== 'production') {
+  MockServer({ environment });
+}
 
 ReactDOM.render(
   <React.StrictMode>
